af643c34f572382655845587c73ece3dd87e4a2c
Jherson Martelo <yesungpapu@gmail.com>
2018-08-25 10:43:33 -0500

    Funcionalidad de la barra de vida lista pero sin diseños o colores aprobado por el diseñador

--- js/Level.js
+++ js/Level.js
@@ -1,11 +1,12 @@
     
     var game = new Phaser.Game(640, 360, Phaser.AUTO, '', { preload: this.preload, create: create, update: update });
 
-    preload: function preload() {
+    function preload() {
 
         game.load.tilemap('keeper', 'assets/Endless_keeper.json', null, Phaser.Tilemap.TILED_JSON);
         game.load.image('tiles', 'assets/warTileset_32x32.png');
         game.load.image('player', 'assets/Keeper32.png');
+        game.load.image('healthbg','assets/healthbg.jpg');
 
     }
 
@@ -13,47 +14,57 @@
     var layer;
     var p;
     var cursors;
+    var myHealthbar;
+    var jump = 0;
 
     function create() {
 
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    game.stage.backgroundColor = '#787878';
+        game.stage.backgroundColor = '#787878';
 
-    map = game.add.tilemap('keeper');
+        map = game.add.tilemap('keeper');
 
-    map.addTilesetImage('EndlessKeeper-World1', 'tiles');
+        map.addTilesetImage('EndlessKeeper-World1', 'tiles');
 
-    map.setCollision(12);
-    map.setCollision(15);
-    map.setCollision(9);
-    map.setTileIndexCallback(9, hitCoin, this);
+        map.setCollision(12);
+        map.setCollision(15);
+        map.setCollision(9);
+        map.setTileIndexCallback(9, hitCoin, this);
     
-    layer = map.createLayer('World1');
+        layer = map.createLayer('World1');
 
-    // layer.debug = true;
+        // layer.debug = true;
 
-    layer.resizeWorld();
+        layer.resizeWorld();
 
-    p = game.add.sprite(32, 200, 'player');
-    game.physics.enable(p);
+        p = game.add.sprite(32, 200, 'player');
+        game.physics.enable(p);
 
-    game.physics.arcade.gravity.y = 350;
+        game.physics.arcade.gravity.y = 350;
 
-    p.body.bounce.y = 0;
-    p.body.linearDamping = 1;
-    p.body.collideWorldBounds = true;
-    game.camera.follow(p);
+        p.body.bounce.y = 0;
+        p.body.linearDamping = 1;
+        p.body.collideWorldBounds = true;
 
-    cursors = game.input.keyboard.createCursorKeys();;
+        var barConfig = {x:550, y:32, flipped:true};
+        myHealthbar = new HealthBar(game, barConfig);
+        myHealthbar.setFixedToCamera(true);
+        myHealthbar.setBarColor('#aaa');
+        game.camera.follow(p);
+
+        cursors = game.input.keyboard.createCursorKeys();
 
     }
     
     function hitCoin(sprite, tile) {
         p.kill();
+        game.state.restart();
         return false;
     }
+    
 
+    
     function update() {
 
         game.physics.arcade.collide(p, layer);
@@ -62,6 +73,15 @@
 
         if (cursors.up.isDown)
         {
+            jump+=0.1;
+            myHealthbar.setPercent(100-jump);
+
+            if(jump>=100){
+                p.kill();
+                game.state.restart();
+                jump=0;
+                return false
+            }
             if (p.body.onFloor())
             {
                 p.body.velocity.y = -160;
@@ -71,12 +91,12 @@
         if (cursors.left.isDown)
         {
             p.body.velocity.x = -90;
-            p.scale.setTo(-1,1);
+            // p.scale.setTo(-1,1);
         }
         else if (cursors.right.isDown)
         {
             p.body.velocity.x = 90;
-            p.scale.setTo(1,1);
+            // p.scale.setTo(1,1);
 
         }
 
