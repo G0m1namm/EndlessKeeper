a04c8fff11acd72d75e1e102b0c978e6f7556d55
Jherson Martelo <yesungpapu@gmail.com>
2018-08-26 19:09:07 -0500

    Joystick funcional, falta diseño

--- js/Level.js
+++ js/Level.js
@@ -1,12 +1,12 @@
     
     var game = new Phaser.Game(640, 360, Phaser.AUTO, '', { preload: this.preload, create: create, update: update });
 
-    preload: function preload() {
+    function preload() {
 
         game.load.tilemap('keeper', 'assets/Endless_keeper.json', null, Phaser.Tilemap.TILED_JSON);
         game.load.image('tiles', 'assets/warTileset_32x32.png');
         game.load.image('player', 'assets/Keeper32.png');
-
+        game.load.spritesheet('gamepad', 'assets/joystick/gamepad_spritesheet.png',100,100);
     }
 
     var map;
@@ -15,7 +15,9 @@
     var cursors;
 
     function create() {
-
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.pageAlignHorizontally = true;
+	game.scale.pageAlignVertically = true;
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     game.stage.backgroundColor = '#787878';
@@ -44,9 +46,16 @@
     p.body.linearDamping = 1;
     p.body.collideWorldBounds = true;
     game.camera.follow(p);
+    cursors = game.input.keyboard.createCursorKeys();
 
-    cursors = game.input.keyboard.createCursorKeys();;
-
+    // Add the VirtualGamepad plugin to the game
+    this.gamepad = this.game.plugins.add(Phaser.Plugin.VirtualGamepad);
+        
+    // Add a joystick to the game (only one is allowed right now)
+    this.joystick = this.gamepad.addJoystick(102, 280,1, 'gamepad');
+    
+    // Add a button to the game (only one is allowed right now)
+    this.button = this.gamepad.addButton(560, 280,0.7, 'gamepad');
     }
     
     function hitCoin(sprite, tile) {
@@ -57,10 +66,9 @@
     function update() {
 
         game.physics.arcade.collide(p, layer);
-
         p.body.velocity.x = 0;
 
-        if (cursors.up.isDown)
+        if (this.button.isDown)
         {
             if (p.body.onFloor())
             {
@@ -68,12 +76,12 @@
             }
         }
 
-        if (cursors.left.isDown)
+        if (this.joystick.properties.left)
         {
             p.body.velocity.x = -90;
             p.scale.setTo(-1,1);
         }
-        else if (cursors.right.isDown)
+        else if (this.joystick.properties.right)
         {
             p.body.velocity.x = 90;
             p.scale.setTo(1,1);
